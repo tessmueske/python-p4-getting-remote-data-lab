df50b1aeb8815f6398e0a42e2bdc05b879e92dab
Tess <tess.mueske@gmail.com>
2024-08-26 12:44:25 -0500

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
+
+        # The GetRequester class should have a get_response_body method that sends a GET request to the URL passed in on initialization. This method should return the body of the response.
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()
+
+        # The GetRequester class should have a load_json method that should use get_response_body to send a request, then return a Python list or dictionary made up of data converted from the response of that request.
